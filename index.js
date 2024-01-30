289c32b341fc2f1622a31c3ab90589dfc2fa76d4
watermc1 <150736079+watermc1@users.noreply.github.com>
2024-01-30 02:47:38 -0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Jugando **Ocean Network**"];
 
 
 let currentIndex = 0;
